0d56e62600f6e5146330c01f2fbfc1270315e354
Rodrigo Navarro <navarrom.rodrigo@gmail.com>
2022-10-23 23:31:08 -0300

    back2bcm

--- teleco.py
+++ teleco.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO                   
 import time                                
 relee = 11                        
-GPIO.setmode(GPIO.BOARD)                  
+GPIO.setmode(GPIO.BCM)                  
 GPIO.setup(relee, GPIO.OUT) 
 GPIO.cleanup()
 
