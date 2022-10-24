e5d5aa665eac199819978418c950d570dd4664c4
Rodrigo Navarro <navarrom.rodrigo@gmail.com>
2022-10-23 23:30:10 -0300

    17-11

--- teleco.py
+++ teleco.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO                   
 import time                                
-relee = 17                         
+relee = 11                        
 GPIO.setmode(GPIO.BOARD)                  
 GPIO.setup(relee, GPIO.OUT) 
 GPIO.cleanup()
