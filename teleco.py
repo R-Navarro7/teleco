2992a76f8d6fcc1fa92d79f97b9f85e890a0cf10
Rodrigo Navarro <navarrom.rodrigo@gmail.com>
2022-10-24 00:00:52 -0300

    protocode

--- teleco.py
+++ teleco.py
@@ -1,8 +1,20 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.setwarnings(False)
-GPIO.setmode (GPIO.BOARD)
-GPIO.setup (8,GPIO.OUT)
+
+def setup():
+    relee, sensor_1, sensor_2 = 11,1,2
+    setup_pin(relee, 0)
+    setup_pin(sensor_1, 1)
+    setup_pin(sensor_2, 1)
+
+def setup_pin(pin,mod):
+    modes = GPIO.OUT, GPIO.IN
+    mode = modes[mod]
+    GPIO.setmode (GPIO.BOARD)
+    GPIO.setup (pin,mode)
+
+
+
 for i in range(10):
     GPIO.output(8,True)
     time.sleep(2)
