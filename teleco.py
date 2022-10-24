8b0ff2dedc12e5f2bff70a6a4d031389be5b5ede
Rodrigo Navarro <navarrom.rodrigo@gmail.com>
2022-10-23 23:35:17 -0300

    cleanups

--- teleco.py
+++ teleco.py
@@ -2,11 +2,13 @@ import RPi.GPIO as GPIO
 import time                                
 relee = 11                        
 GPIO.setmode(GPIO.BCM)                  
-GPIO.setup(relee, GPIO.OUT) 
+GPIO.setup(relee, GPIO.OUT)
+GPIO.setwarnings(False) 
 GPIO.cleanup()
 
 while True:                              
     GPIO.output(relee, True)      
     time.sleep(1)   
     GPIO.output(relee, False)   
-    time.sleep(1)                   
+    time.sleep(1)  
+    GPIO.cleanup()                 
