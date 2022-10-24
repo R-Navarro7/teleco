ec67aaeebfd7dc18881fc3be0c0ec6c44abf905d
Rodrigo Navarro <navarrom.rodrigo@gmail.com>
2022-10-23 23:43:12 -0300

    whilfe2for

--- teleco.py
+++ teleco.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode (GPIO.BOARD)
 GPIO.setup (8,GPIO.OUT)
-while True:
+for i in range(10):
     GPIO.output(8,True)
     time.sleep(2)
     GPIO.output(8,False)
