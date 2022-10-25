2183d4ee833e9ab5c4b6c9216c15e0401a3fb8a3
R-Navarro7 <ignacio.romero.a@ug.uchile.cl>
2022-10-25 15:38:52 -0300

    small fix

--- teleco.py
+++ teleco.py
@@ -6,8 +6,8 @@ pins = [11,1,2] # relay, sensor_1, sensor_2
 def setup(pins):
     
     setup_pin(pins[0], 0)
-    setup_pin(pins[1], 1)
-    setup_pin([pins[2]], 1)
+    #setup_pin(pins[1], 1)
+    #setup_pin([pins[2]], 1)
 
 def setup_pin(pin,mode_input):
     gpio_modes = GPIO.OUT, GPIO.IN
