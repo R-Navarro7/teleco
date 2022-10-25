a50a0c3f590f0ea1a499b262664cc98cdb18ee2a
R-Navarro7 <ignacio.romero.a@ug.uchile.cl>
2022-10-25 16:40:40 -0300

    small fix

--- teleco.py
+++ teleco.py
@@ -6,10 +6,10 @@ import time
 
 pins = [11,16,2] # relay, sensor_1, sensor_2
 
-def setup(pins):
+#def setup(pins):
     
     #setup_pin(pins[0], 0)
-    setup_pin(pins[1], 1)
+    #setup_pin(pins[1], 1)
     #setup_pin([pins[2]], 1)
 
 def setup_pin(pin,mode_input):
@@ -18,7 +18,7 @@ def setup_pin(pin,mode_input):
     GPIO.setmode (GPIO.BOARD)
     GPIO.setup (pin,mode)
 
-setup(pins)
+#setup(pins)
 
 for proc in psutil.process_iter():
     if proc.name() == 'libgpiod_pulsein' or proc.name() == 'libgpiod_pulsei':
