ffd74a97b10526ced0caf066399f6466c73191e1
R-Navarro7 <navarrom.rodrigo@gmail.com>
2022-12-09 16:02:26 -0300

    aaaa

--- telecoweb/iot/views.py
+++ telecoweb/iot/views.py
@@ -19,7 +19,7 @@ def update(request):
     m_len = len(meassure_list)
     plot_list = meassure_list[m_len-30:]
     context = {"meassure_list" : meassure_list, "plot_list": plot_list}
-    return render(request, 'iot/chart.html', context)
+    return render(request, 'iot/test.html', context)
 
 @csrf_exempt
 def add_meassure(request):
